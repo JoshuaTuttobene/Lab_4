095a3f7e770d800efb818de8c096bbb1edffc7a3
aaron-esc-13 <156145840+aaron-esc-13@users.noreply.github.com>
2024-02-28 14:54:26 -0800

    updated

--- src/lab_4_plots.py
+++ src/lab_4_plots.py
@@ -1,9 +1,8 @@
 """!
 @file lab_4_plots.py
-Run a motor step response based on a user inputted Kp. This program cals the
-microcontroller to run and get data before plotting the closed loop PController step response.
-It uses Tkinter, an old-fashioned and ugly but useful GUI library which is included in Python by
-default.
+Run a motor step response for two motors based on user inputted Kps and setpoints. 
+This program calls the microcontroller to run and get get data before plotting 
+the closed loop PController step response for both motors.
 
 @author Aaron Escamilla, Karen Morales De Leon, Joshua Tuttobene
 @date   02/29/2024 Original program, based on example from above listed source
@@ -32,16 +31,37 @@ p2_data = []
 
     
  # Takes user input for Kp and setpoint and then sends to microcontroller
-ser.write(bytes(input("Enter a Kp_1 value:"),'utf-8')) # Kp for motor 1
+while True:
+    try:
+        kp_1 = input("Enter a Kp_1 value:")
+        kp_1_t = float(kp_1)
+        if kp_1_t <= 0:
+            raise ValueError
+        sp_1 = input("Enter setpoint 1:")
+        sp_1_t = float(sp_1)
+        if sp_1_t <= 0:
+            raise ValueError
+        kp_2 = input("Enter a Kp_2 value:")
+        kp_2_t = float(kp_2)
+        if kp_2_t <= 0:
+            raise ValueError
+        sp_2 = input("Enter setpoint 2:")
+        sp_2_t = float(sp_2)
+        if sp_2_t <= 0:
+            raise ValueError
+        break
+    except (ValueError, TypeError):
+        print('enter a positive numerical value')
+ser.write(kp_1.encode('Ascii'))#bytes(kp_1,'utf-8')) # Kp for motor 1
 ser.write(b'\r')
 
-ser.write(bytes(input("Enter setpoint 1:"),'utf-8')) # setpoint for motor 1
+ser.write(bytes(sp_1,'utf-8')) # setpoint for motor 1
 ser.write(b'\r')
 
-ser.write(bytes(input("Enter a Kp_2 value:"),'utf-8')) # Kp for motor 2
+ser.write(bytes(kp_2,'utf-8')) # Kp for motor 2
 ser.write(b'\r')
 
-ser.write(bytes(input("Enter setpoint 2:"),'utf-8')) # setpoint for motor 2
+ser.write(bytes(sp_2,'utf-8')) # setpoint for motor 2
 ser.write(b'\r')
 
 # loop to get data from serial port for encoder 1 until 'end' is printed, signifying the end of data
